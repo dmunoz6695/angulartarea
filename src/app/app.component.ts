80bc61db85ba629dca91af4e1ffcf2558c89115a
DM <difermu02@gmail.com>
2022-03-27 23:54:15 -0500

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,72 @@
 import { Component } from '@angular/core';
 
+import { Country } from './models/country';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'parcialenfasis';
+
+
+
+  countryArray: Country[] = [
+
+    {id: 1, country: "Colombia", city: "Barranquilla"},
+    {id: 2, country: "México", city: "Monterrey"},
+    {id: 3, country: "Argentina", city: "Buenos Aires"},
+
+  ];
+
+  selectedCountry: Country = new Country ();
+  selected2Country: Country = new Country ();
+
+  openForEdit(country : Country){
+
+    this.selectedCountry = country;
+    
+
+  }
+  
+  delete(country : Country){
+
+    this.selected2Country = country;
+    
+    if (confirm('Seguro que quiere eliminar esta información?')  ) {
+    
+      this.countryArray = this.countryArray.filter(i => i != this.selected2Country);
+      
+      this.selected2Country = new Country();
+
+
+      }
+
+
+
+  }
+
+
+
+  
+
+
+  addOrEdit(){
+
+    if (this.selectedCountry.id === 0 ) {
+      this.selectedCountry.id = this.countryArray.length + 1;
+      this.countryArray.push(this.selectedCountry);
+
+    }
+
+
+
+    this.selectedCountry = new Country();
+
+    
+    
+  }
+
+
 }
---
+++ src/app/models/country.ts
@@ -0,0 +1,7 @@
+export class Country {
+
+    id : number = 0;
+    country : string;
+    city : string;
+
+}
